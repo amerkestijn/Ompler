a142dc26079bd75de281b7273af5373efb4b5185
Aron Merkestijn <aron@merkestijn.io>
2022-08-30 21:14:32 +0200

    test route

--- routes/calls.js
+++ routes/calls.js
@@ -7,6 +7,10 @@ const {
 
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.status(200).json("works");
+});
+
 router.post("/", manualUsernameAdd);
 
 router.get("/:username", getPronounByUsername);
