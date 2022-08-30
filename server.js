b23c49b4209d0ad3a823e29ed143b0a1dc23da17
Aron Merkestijn <aron@merkestijn.io>
2022-08-30 18:29:59 +0200

    idk

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
 
 const express = require("express");
 const apiRoutes = require("./routes/calls");
